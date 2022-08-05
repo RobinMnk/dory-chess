59b756c87fcca84a4311b9580ea487d96d780477
Robin Münk <RobinMnk@users.noreply.github.com>
2022-08-05 19:49:35 +0200

    moved util functions to header file

--- src/utils.h
+++ src/utils.h
@@ -23,30 +23,136 @@ namespace Utils {
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'
     };
 
-    void print_board(Board& board);
+    constexpr char filename(int file) {
+        return FILE_NAMES.at(file);
+    }
 
-    void print_board(ByteBoard&& byteboard);
+    std::string squarename(int file, int rank) {
+        std::ostringstream oss;
+        oss << filename(file) << (rank + 1);
+        return oss.str();
+    }
 
-    ByteBoard to_byteboard(Board board);
+    std::string squarename(BB board) {
+        int index = singleBitOf(board);
+        return squarename(fileOf(index), rankOf(index));
+    }
+
+
+    void add_piece(ByteBoard &byteboard, BB pieces, char letter) {
+        BB selector = 1;
+        for (int i = 0; i < 64; i++) {
+            if (pieces & selector) {
+                if (byteboard[i]) {
+                    throw std::runtime_error(
+                            "two pieces on " + squarename(newMask(i)) + ": " + static_cast<char>(byteboard[i]) +
+                            " and " + letter);
+                }
+                byteboard[i] = letter;
+            }
+            selector <<= 1;
+        }
+    }
+
+    ByteBoard to_byteboard(Board board) {
+        ByteBoard byteboard{};
+        add_piece(byteboard, board.wPawns, 'P');
+        add_piece(byteboard, board.bPawns, 'p');
+        add_piece(byteboard, board.wKnights, 'N');
+        add_piece(byteboard, board.bKnights, 'n');
+        add_piece(byteboard, board.wBishops, 'B');
+        add_piece(byteboard, board.bBishops, 'b');
+        add_piece(byteboard, board.wRooks, 'R');
+        add_piece(byteboard, board.bRooks, 'r');
+        add_piece(byteboard, board.wQueens, 'Q');
+        add_piece(byteboard, board.bQueens, 'q');
+        add_piece(byteboard, board.wKing, 'K');
+        add_piece(byteboard, board.bKing, 'k');
+        return byteboard;
+    }
+
+    void print_board(ByteBoard &&byteboard) {
+        int rank = 7;
+        while (rank >= 0) {
+            for (int i = 0; i < 8; i++) {
+                uint8_t letter = byteboard.at(i + rank * 8);
+                if (letter != 0) std::cout << letter << " ";
+                else std::cout << "  ";
+            }
+            std::cout << "\n";
+            rank--;
+        }
+        std::cout << std::endl;
+    }
+
+    std::string specialMove(Flag_t flags) {
+        switch (flags) {
+            case MoveFlag::PromoteBishop: return "=B";
+            case MoveFlag::PromoteKnight: return "=N";
+            case MoveFlag::PromoteRook: return "=R";
+            case MoveFlag::PromoteQueen: return "=Q";
+            case MoveFlag::ShortCastling: return "0-0";
+            case MoveFlag::LongCastling: return "0-0-0";
+            default: return "";
+        }
+    }
+    std::string pieceString(Piece_t piece) {
+        switch (piece) {
+            case Piece::King: return "K";
+            case Piece::Queen: return "Q";
+            case Piece::Rook: return "R";
+            case Piece::Bishop: return "B";
+            case Piece::Knight: return "N";
+            default: return "";
+        }
+    }
+
+    void printMove(Move m) {
+        if(m.flags == MoveFlag::ShortCastling) std::cout << specialMove(MoveFlag::ShortCastling) << std::endl;
+        else if(m.flags == MoveFlag::LongCastling) std::cout << specialMove(MoveFlag::LongCastling) << std::endl;
+        else std::cout << pieceString(m.piece) << squarename(m.from) << "-" << squarename(m.to) << specialMove(m.flags) << std::endl;
+    }
+
+    void print_board(Board &board) {
+        print_board(to_byteboard(board));
+    }
+
+    void printOcc(BB occ) {
+        int rank = 7;
+        while (rank >= 0) {
+            for (int file = 0; file < 8; file++) {
+                if (hasBitAt(occ, 8 * rank + file))
+                    std::cout << squarename(file, rank) << " ";
+                else std::cout << "  ";
+            }
+            std::cout << "\n";
+            rank--;
+        }
+        std::cout << std::endl;
+    }
+
+    int sqId(std::string_view name) {
+        int file{0};
+        for (char c: FILE_NAMES) {
+            if (name.at(0) == c) break;
+            file++;
+        }
+        int rank{name.at(1) - '0' - 1};
+        return 8 * rank + file;
+    }
+
+    BB sqBB(std::string &name) {
+        return newMask(sqId(name));
+    }
+
+    BB sqBB(std::string &&name) {
+        return newMask(sqId(name));
+    }
 
     template<bool white>
     constexpr char piece_as_char(uint8_t piece) {
         return PIECE_TO_CHAR.at(white ? piece : (piece + 7));
     }
-
-    constexpr char filename(int file) {
-        return FILE_NAMES.at(file);
-    }
-
-    std::string squarename(int file, int rank);
-
-    std::string squarename(BB board);
-
-    std::string specialMove(Flag_t flags);
-
-    std::string pieceString(Piece_t piece);
-
-    void printMove(Move);
 
     template<bool whiteMoving>
     std::string moveName(Move m) {
@@ -62,13 +168,6 @@ namespace Utils {
         bss << squarename(m.from) << squarename(m.to);
         return bss.str();
     }
-
-    void printOcc(BB occ);
-
-    int sqId(std::string_view name);
-
-    BB sqBB(std::string& name);
-    BB sqBB(std::string&& name);
 
     template<typename Collector, State state, int depth>
     void time_movegen(Board& board) {
